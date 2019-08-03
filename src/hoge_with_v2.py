72819c76b0f021df36cb69c0dbfbc77e227796ca
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-08-03 09:21:58 +0900

    クラスを追加する

--- src/hoge_with_v2.py
+++ src/hoge_with_v2.py
@@ -4,3 +4,7 @@ import hogelib.v2 as hogelib
 
 if __name__ == '__main__':
   print( hogelib.version() )
+
+  obj = hogelib.SomeClass()
+  print( obj.foo() )
+  print( obj.specialMethodForV2() )
---
+++ src/hogelib/v2/some_class.py
@@ -0,0 +1,5 @@
+class SomeClass:
+  def foo(self):
+    return "SomeCass.foo for v2"
+  def specialMethodForV2(self):
+    return "SomeCass.specialMethodForV2"
